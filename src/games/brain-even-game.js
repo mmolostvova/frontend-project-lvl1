1d907f67ee02514a945605503e3bc569a9e37da2
Marina <mmolostvova@gmail.com>
2021-03-15 13:35:13 +0200

    move game engine to index.js, even-game is working

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,12 +1,23 @@
 // import readline from 'readline-sync';
-import { welcomeMessage, showRules, gameFlow } from '../index.js';
+import { welcomeMessage, showRules, askQgetA, isAnswerRight } from '../index.js';
 
 export default () => {
-  const name = welcomeMessage('Welcome to the Brain Games!', 'May I have your name? ');
+  const userName = welcomeMessage();
   showRules('Answer "yes" if the number is even, otherwise answer "no".');
-  const randomNumber = Math.floor(Math.random() * 101);
-  const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
-  gameFlow(randomNumber, rightAnswer, name);
+  let winsCounter = 0;
+  do {
+    const randomNumber = Math.floor(Math.random() * 101);
+    const userAnswer = askQgetA(randomNumber);
+    const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+    if (isAnswerRight(userAnswer, rightAnswer)) {
+      console.log('Correct!');
+      winsCounter += 1;
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}. Let's try again, ${userName}!`);
+      winsCounter = 0;
+    }
+  } while (winsCounter < 3);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 // export default () => {
@@ -41,6 +52,7 @@ export default () => {
 //       const stringAnswer = a ? "'yes'" : "'no'";
 //       const stringUserAnswer = userAnswer ? "'yes'" : "'no'";
 //       console.log(`${stringUserAnswer} is wrong answer ;(. Correct answer was ${stringAnswer}. Let's try again, ${userName}!`);
+//       winsCounter = 0;
 //     }
 //   } while (winsCounter < 3);
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readline from 'readline-sync';
 
-export const welcomeMessage = (welcomeText, askingNameText) => {
-  console.log(welcomeText);
-  const userName = readline.question(askingNameText);
+export const welcomeMessage = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readline.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 };
@@ -11,18 +11,12 @@ export const showRules = (rules) => {
   console.log(rules);
 };
 
-export const gameFlow = (question, rightAnswer, userName) => {
-  let winsCounter = 0;
-  do {
-    const userAnswer = readline.question(`Question: ${question}`);
-    console.log(`Your answer: ${userAnswer}`);
-    if (userAnswer === rightAnswer) {
-      console.log('Correct!');
-      winsCounter += 1;
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}. Let's try again, ${userName}!`);
-      winsCounter = 0;
-    }
-  } while (winsCounter < 3);
-  console.log(`Congratulations, ${userName}!`);
+export const askQgetA = (question) => {
+  const userAnswer = readline.question(`Question: ${question}`);
+  console.log(`Your answer: ${userAnswer}`);
+  return userAnswer;
 };
+
+export function isAnswerRight(userAnswer, rightAnswer) {
+  return userAnswer === rightAnswer;
+}
