6e596a0d9162c8d42cd24cc6bdbeb754302e8166
Marina <mmolostvova@gmail.com>
2021-03-30 11:40:54 +0300

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-// import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
-// export default () => {
-//   const userName = readlineSync.question('May I have your name? ');
-//   console.log(`Hello, ${userName}!`);
-// };
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
 export const getRandomNumber = (maxNumber) => {
   const randomNumber = Math.floor(Math.random() * maxNumber);
