b870f7c027c675df06f99198f9db3b0edf8475ae
Marina <mmolostvova@gmail.com>
2021-03-31 17:48:34 +0300

    small fix

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,6 +1,6 @@
 import readline from 'readline-sync';
 
-import getRandomNumber from '../calculations.js';
+import { getRandomNumber } from '../calculations.js';
 
 const isEven = (number) => number % 2 === 0;
 
