85f379608d2b6b1076da5efc6ee94fb0c526efe5
Marina <mmolostvova@gmail.com>
2021-04-15 16:40:05 +0300

    try to implement game engine (bugs)

---
+++ src/gameEngine.js
@@ -0,0 +1,24 @@
+import readline from 'readline-sync';
+
+export default (rules, getQuestion, getRightAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readline.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rules);
+
+  for (let i = 0; i < 3; i += 1) {
+    const quest = getQuestion();
+    console.log(`Question: ${quest}`);
+    const userAnswer = readline.question('Your answer: ');
+    const rightAnswer = getRightAnswer(quest);
+
+    if (userAnswer !== rightAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}. Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,28 +1,35 @@
-import readline from 'readline-sync';
-
+import game from '../gameEngine.js'
 import getRandomNumber from '../utils.js';
 
 const isEven = (number) => number % 2 === 0;
 
-export default () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readline.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = getRandomNumber(45, 101);
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = readline.question('Your answer: ');
-    const rightAnswer = isEven(randomNumber) ? 'yes' : 'no';
+const getQuestion = () => getRandomNumber(45, 101);
 
-    if (userAnswer !== rightAnswer) {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}. Let's try again, ${userName}!`);
-      return;
-    }
+const getRightAnswer = (question) => (isEven(question) ? 'yes' : 'no');
 
-    console.log('Correct!');
-  }
+export default game(rules, getQuestion, getRightAnswer);
 
-  console.log(`Congratulations, ${userName}!`);
-};
+// export default () => {
+//   console.log('Welcome to the Brain Games!');
+//   const userName = readline.question('May I have your name? ');
+//   console.log(`Hello, ${userName}!`);
+//   console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+//   for (let i = 0; i < 3; i += 1) {
+//     const randomNumber = getRandomNumber(45, 101);
+//     console.log(`Question: ${randomNumber}`);
+//     const userAnswer = readline.question('Your answer: ');
+//     const rightAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+//     if (userAnswer !== rightAnswer) {
+//       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}. Let's try again, ${userName}!`);
+//       return;
+//     }
+
+//     console.log('Correct!');
+//   }
+
+//   console.log(`Congratulations, ${userName}!`);
+// };
