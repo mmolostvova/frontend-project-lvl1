327f26a7dd4d10958d857c890cd59420f454e594
Marina <mmolostvova@gmail.com>
2021-04-16 02:37:23 +0300

    fix progression game

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -16,7 +16,7 @@ export default () => {
       return progression;
     };
     const numbers = createProgression(10);
-    const indexOfEmptyElement = getRandomNumber(1, 8);
+    const indexOfEmptyElement = getRandomNumber(-1, 9);
     numbers.splice(indexOfEmptyElement, 1, '..');
 
     return numbers.join(' ');
@@ -25,8 +25,20 @@ export default () => {
   const getRightAnswer = (question) => {
     const numbers = question.split(' ');
     const i = numbers.indexOf('..');
-    const step = (numbers[i + 1] - numbers[i - 1]) / 2;
-    const cutNumber = Number(numbers[i - 1]) + Number(step);
+    const lastIndex = numbers.length - 1;
+    let cutNumber;
+    if (i === 0 || i === lastIndex) {
+      const step = (numbers[2] - numbers[1]) / 2;
+      if (i === 0) {
+        cutNumber = Number(numbers[1]) - Number(step);
+      } else {
+        cutNumber = Number(numbers[lastIndex]) + Number(step);
+      }
+    } else {
+      const step = (numbers[i + 1] - numbers[i - 1]) / 2;
+      cutNumber = Number(numbers[i - 1]) + Number(step);
+    }
+
     return cutNumber.toString();
   };
 
