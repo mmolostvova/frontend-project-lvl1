1f090bafd1685eba77f30f7988dff71ea45166a0
Marina <mmolostvova@gmail.com>
2021-04-25 16:59:31 +0300

    refactor progression and gcd games

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -1,20 +1,20 @@
-import game from '../gameEngine.js';
+import play from '../index.js';
 import getRandomNumber from '../utils.js';
+
+const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
 
 export default () => {
   const rules = 'Find the greatest common divisor of given numbers.';
 
-  const getQuestion = () => {
+  const generateRound = () => {
     const number1 = getRandomNumber(1, 15);
     const number2 = getRandomNumber(1, 15);
-    return `${number1} ${number2}`;
+
+    return {
+      question: `${number1} ${number2}`,
+      rightAnswer: getGCD(number1, number2).toString(),
+    };
   };
 
-  const getRightAnswer = (question) => {
-    const numbers = question.split(' ');
-    const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
-    return getGCD(numbers[0], numbers[1]).toString();
-  };
-
-  game(rules, getQuestion, getRightAnswer);
+  play(rules, generateRound);
 };
--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -1,46 +1,30 @@
-import game from '../gameEngine.js';
+import play from '../index.js';
 import getRandomNumber from '../utils.js';
 
+const createProgression = (progressionLength) => {
+  const firstEl = getRandomNumber(20, 40);
+  const progression = new Array(progressionLength);
+  progression[0] = firstEl;
+  const step = getRandomNumber(5, 14);
+  for (let i = 1; i < progressionLength; i += 1) {
+    progression[i] = progression[i - 1] + step;
+  }
+  return progression;
+};
+
 export default () => {
-  const rules = 'What number is missing in the progression?';
+  const description = 'What number is missing in the progression?';
 
-  const getQuestion = () => {
-    const createProgression = (progressionLength) => {
-      const firstEl = getRandomNumber(20, 40);
-      const progression = new Array(progressionLength);
-      progression[0] = firstEl;
-      const step = getRandomNumber(5, 14);
-      for (let i = 1; i < progressionLength; i += 1) {
-        progression[i] = progression[i - 1] + step;
-      }
-      return progression;
-    };
+  const generateRound = () => {
     const numbers = createProgression(10);
-    const indexOfEmptyElement = getRandomNumber(-1, 9);
-    numbers.splice(indexOfEmptyElement, 1, '..');
+    const indexOfEmptyElement = getRandomNumber(0, 9);
+    const cutNumber = numbers.splice(indexOfEmptyElement, 1, '..');
 
-    return numbers.join(' ');
+    return {
+      question: numbers.join(' '),
+      rightAnswer: cutNumber.toString(),
+    };
   };
 
-  const getRightAnswer = (question) => {
-    const numbers = question.split(' ');
-    const i = numbers.indexOf('..');
-    const lastIndex = numbers.length - 1;
-    let cutNumber;
-    if (i === 0 || i === lastIndex) {
-      const step = (numbers[2] - numbers[1]) / 2;
-      if (i === 0) {
-        cutNumber = Number(numbers[1]) - Number(step);
-      } else {
-        cutNumber = Number(numbers[lastIndex]) + Number(step);
-      }
-    } else {
-      const step = (numbers[i + 1] - numbers[i - 1]) / 2;
-      cutNumber = Number(numbers[i - 1]) + Number(step);
-    }
-
-    return cutNumber.toString();
-  };
-
-  game(rules, getQuestion, getRightAnswer);
+  play(description, generateRound);
 };
